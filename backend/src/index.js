255fc6180965fc80c5cb2c8953a0f49cdd44464f
ankankumar123 <ankankumar257@gmail.com>
2026-02-17 20:49:37 +0530

    login button added at tracker board

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const incidentRoutes  =require('./modules/incidentTracker/routes/incident.routes
 
 // CORS
 app.use(cors({
-  origin:'http://localhost:5173', 
+  origin:process.env.CLIENT_URL, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,7 +9,14 @@ function HomePage() {
     <div className="min-h-screen bg-gray-100 px-4 sm:px-6 lg:px-8 py-6">
       
    <div className="max-w-7xl mx-auto flex items-center justify-between mb-6">
-
+     
+     <button onClick={()=> navigate("/login")} 
+          className="bg-gray-600 text-white 
+               px-3 sm:px-4 py-2 rounded-lg 
+               hover:bg-gray-500 transition text-sm sm:text-base"
+     >
+      Login
+    </button>
   <h1 className="text-xl sm:text-2xl lg:text-3xl font-semibold text-gray-800">
     Incident Tracker
   </h1>
